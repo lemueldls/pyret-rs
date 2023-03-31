ba7d1a2d3e2bef3e824697667a490d87425ea094
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-03-31 19:14:36 -0400

    ♻️ Wasm refactor

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,11 +1,16 @@
 #![allow(clippy::use_self, clippy::new_without_default, clippy::boxed_local)]
 
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc, sync::Arc};
 
-use js_sys::Function;
-use pyret_interpreter::{trove, value::PyretValue, Interpreter, PyretGraph};
+use js_sys::{Array, Function, Object, Reflect};
+use pyret_error::PyretResult;
+use pyret_file::PyretFile;
+use pyret_interpreter::{
+    trove,
+    value::{context::Context, PyretFunction, PyretValue},
+    Interpreter, PyretGraph,
+};
 use pyret_number::{BigRational, PyretNumber};
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "console_error_panic_hook")]
@@ -13,14 +18,6 @@ use wasm_bindgen::prelude::*;
 #[inline]
 pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(tag = "kind", content = "value")]
-#[non_exhaustive]
-pub enum PyretValueWrapper {
-    ExactNumber(BigRational),
-    RoughNumber(f64),
 }
 
 #[wasm_bindgen]
@@ -35,7 +32,7 @@ impl PyretGraph for PyretGraphWrapper {
         todo!()
     }
 
-    fn get(&self, _file_id: usize) -> &pyret_interpreter::PyretFile {
+    fn get(&self, _file_id: usize) -> &PyretFile {
         todo!()
     }
 }
@@ -45,117 +42,114 @@ impl PyretRuntime {
     #[wasm_bindgen(constructor)]
     #[must_use]
     pub fn new() -> Self {
-        let interpreter = Interpreter::new(PyretGraphWrapper);
-
-        interpreter.use_context::<trove::Global>();
+        let mut interpreter = Interpreter::new(PyretGraphWrapper);
 
         Self { interpreter }
     }
 
-    pub fn eval(&mut self, _source: &str) -> Box<[JsValue]> {
-        let values = self.interpreter.interpret(0).unwrap();
-
-        values
-            .iter()
-            .map(|value| serde_wasm_bindgen::to_value(&PyretValueWrapper::from(value)).unwrap())
-            .collect()
+    #[wasm_bindgen(js_name = "useContext")]
+    pub fn use_context(&mut self, context: &str) {
+        self.interpreter.import_trove(context).unwrap();
     }
 
-    pub fn call_function(&mut self, name: &str, args: Box<[JsValue]>) -> JsValue {
-        let binding = self.interpreter.context.borrow_mut();
+    pub fn run(&mut self, file_id: usize) -> Box<[JsValue]> {
+        let values = self.interpreter.interpret(file_id).unwrap();
 
-        let function = binding.registrar.get(name).unwrap();
-
-        serde_wasm_bindgen::to_value(&PyretValueWrapper::from(&match &***function {
-            PyretValue::Function(function) => function
-                .call(
-                    &args
-                        .iter()
-                        .map(|arg| {
-                            serde_wasm_bindgen::from_value::<PyretValueWrapper>(arg.clone())
-                                .unwrap()
-                                .into()
-                        })
-                        .collect::<Vec<Rc<PyretValue>>>(),
-                    Rc::clone(&self.interpreter.context),
-                )
-                .unwrap(),
-
-            _ => todo!(),
-        }))
-        .unwrap()
+        values.iter().map(pyret_to_js).collect()
     }
-
-    pub fn register_function(&mut self, name: &str, body: Function) {
-        self.interpreter
-            .context
-            .borrow_mut()
-            .registrar
-            .register_local_function(
-                name,
-                body.length() as usize,
-                Box::new(move |args, _context| {
-                    let value = body
-                        .apply(
-                            &JsValue::NULL,
-                            &args
-                                .iter()
-                                .map(|arg| {
-                                    serde_wasm_bindgen::to_value(&PyretValueWrapper::from(arg))
-                                        .unwrap()
-                                })
-                                .collect(),
-                        )
-                        .unwrap();
-
-                    if value.is_falsy() {
-                        todo!("Must return a value")
-                    }
-
-                    let wrapper: PyretValueWrapper = serde_wasm_bindgen::from_value(value).unwrap();
-
-                    Ok(wrapper.into())
-                }),
-            );
-    }
-
-    // pub fn execute(&mut self, wrapper: JsValue) -> PyretValueWrapper {
-    //     match wrapper.kind {
-    //         _ => wrapper,
-    //     }
-    // }
-
-    // #[wasm_bindgen(js_name = getValue)]
-    // pub fn get_value(&mut self, ident: &str) -> Option<PyretValueWrapper> {
-    //     Some(wrap(&**self.interpreter.register.get(ident)?))
-    // }
 }
 
-impl From<&Rc<PyretValue>> for PyretValueWrapper {
-    fn from(value: &Rc<PyretValue>) -> Self {
-        match &**value {
-            PyretValue::Number(number) => match number {
-                PyretNumber::Exact(exact_number) => {
-                    PyretValueWrapper::ExactNumber(exact_number.clone())
-                }
+#[must_use]
+fn pyret_to_js(value: &Rc<PyretValue>) -> JsValue {
+    let (tag, value) = match &**value {
+        PyretValue::Number(number) => match number {
+            PyretNumber::Exact(exact) => {
+                ("Exactnum", serde_wasm_bindgen::to_value(&exact).unwrap())
+            }
+            PyretNumber::Rough(rough) => ("Roughnum", JsValue::from_f64(*rough)),
+        },
+        PyretValue::String(string) => ("String", JsValue::from_str(string)),
+        PyretValue::Boolean(boolean) => ("Boolean", JsValue::from_bool(*boolean)),
+        PyretValue::Function(function) => {
+            let function = function.clone();
 
-                PyretNumber::Rough(rough_number) => PyretValueWrapper::RoughNumber(*rough_number),
-            },
+            let closure = Closure::wrap(Box::new(move |args: Vec<JsValue>| {
+                let args = args
+                    .into_iter()
+                    .map(|value| Rc::new(js_to_pyret(value)))
+                    .collect::<Box<[Rc<PyretValue>]>>();
 
-            _ => todo!(),
+                let value = function.call(&args, 0).unwrap();
+
+                pyret_to_js(&value)
+            }) as Box<dyn FnMut(Vec<JsValue>) -> JsValue>);
+
+            ("Function", closure.into_js_value())
         }
-    }
+        PyretValue::Nothing => ("Nothing", JsValue::UNDEFINED),
+    };
+
+    let object = Object::new();
+
+    Reflect::set(&object, &"tag".into(), &tag.into()).unwrap();
+    Reflect::set(&object, &"value".into(), &value).unwrap();
+
+    JsValue::from(object)
 }
 
-impl From<PyretValueWrapper> for Rc<PyretValue> {
-    fn from(wrapper: PyretValueWrapper) -> Self {
-        Rc::new(match wrapper {
-            PyretValueWrapper::ExactNumber(exact_number) => {
-                PyretValue::Number(PyretNumber::Exact(exact_number))
-            }
-            PyretValueWrapper::RoughNumber(rough_number) => {
-                PyretValue::Number(PyretNumber::Rough(rough_number))
-            }
-        })
+fn js_to_pyret(value: JsValue) -> PyretValue {
+    let object = value.dyn_into::<Object>().unwrap();
+
+    let tag = Reflect::get(&object, &"tag".into())
+        .unwrap()
+        .as_string()
+        .unwrap();
+
+    let value = Reflect::get(&object, &"value".into()).unwrap();
+
+    match tag.as_str() {
+        "Exactnum" => PyretValue::Number(PyretNumber::Exact(
+            serde_wasm_bindgen::from_value(value).unwrap(),
+        )),
+        "Roughnum" => PyretValue::Number(PyretNumber::Rough(value.as_f64().unwrap())),
+        "String" => PyretValue::String(value.as_string().unwrap().into_boxed_str()),
+        "Boolean" => PyretValue::Boolean(value.as_bool().unwrap()),
+        "Function" => {
+            let function = value.dyn_into::<Function>().unwrap();
+
+            let name = function.name().as_string().unwrap().into_boxed_str();
+
+            let generics = (0..function.length())
+                .map(|_| Box::from("Any"))
+                .collect::<Vec<_>>()
+                .into_boxed_slice();
+
+            let any = trove::global::Any::predicate();
+
+            let param_types = (0..function.length())
+                .map(|_| Arc::clone(&any))
+                .collect::<Vec<_>>()
+                .into_boxed_slice();
+
+            let body = Rc::new(move |args: &[Rc<PyretValue>], _context| {
+                let args = args.iter().map(pyret_to_js).collect::<Array>();
+
+                let value = function.apply(&JsValue::UNDEFINED, &args).unwrap();
+
+                Ok(Rc::new(js_to_pyret(value)))
+            });
+
+            let context = Rc::new(RefCell::new(Context::default()));
+
+            PyretValue::Function(PyretFunction::new(
+                name,
+                generics,
+                param_types,
+                any,
+                body,
+                context,
+            ))
+        }
+        _ => todo!(),
     }
 }
